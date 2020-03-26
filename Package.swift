b3a49f4a9fbee3c7320591dbc7263c192244063e
fwcd <fwcdmail@gmail.com>
2020-03-26 01:48:45 +0100

    Update swift-tools-version to 5.1 and add library product

--- Package.swift
+++ Package.swift
@@ -1,8 +1,11 @@
-// swift-tools-version:4.2.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "Emoji",
+    products: [
+        .library(name: "Emoji", targets: ["Emoji"])
+    ],
     targets: [
         .target(
             name: "Emoji"
