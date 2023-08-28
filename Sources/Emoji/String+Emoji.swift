b71a2d95201f47e23c04d9bca9086451e499eea7
Yuji MATSUMOTO <matsumoto@nulab.com>
2023-08-28 20:33:20 +0900

    refactor

--- Sources/Emoji/String+Emoji.swift
+++ Sources/Emoji/String+Emoji.swift
@@ -13,15 +13,16 @@ extension String {
 
     fileprivate static var emojiUnescapeRegExp = createEmojiUnescapeRegExp()
     fileprivate static var emojiEscapeRegExp   = createEmojiEscapeRegExp()
-    fileprivate static var indexedShortnames   = indexShortnames()
-    fileprivate static var indexedCodepoints   = indexCodepoints()
-
+    fileprivate static var indexedShortnames   = createIndex(for: \.shortnames)
+    fileprivate static var indexedCodepoints   = createIndex(for: \.codepoints)
+   
     fileprivate static func createEmojiUnescapeRegExp() -> NSRegularExpression? {
-        let v = Emoji.allCases.flatMap { $0.shortnames }
+        let pattern = Emoji.allCases
+            .flatMap { $0.shortnames }
             .map { ":\(NSRegularExpression.escapedPattern(for: $0)):" }
+            .joined(separator: "|")
         do {
-            let regex = try NSRegularExpression(pattern: v.joined(separator: "|"), options: [])
-            return regex
+            return try NSRegularExpression(pattern: pattern, options: [])
         } catch {
             print(error)
         }
@@ -29,43 +30,31 @@ extension String {
     }
 
     fileprivate static func createEmojiEscapeRegExp() -> NSRegularExpression? {
-        let v = Emoji.allCases.flatMap { $0.codepoints }
+        let pattern = Emoji.allCases
+            .flatMap { $0.codepoints }
             .map { NSRegularExpression.escapedPattern(for: $0) }
             .sorted()
             .reversed()
+            .joined(separator: "|")
         do {
-            let regex = try NSRegularExpression(pattern: v.joined(separator: "|"), options: [])
-            return regex
+            return try NSRegularExpression(pattern: pattern, options: [])
         } catch {
             print(error)
         }
         return nil
     }
-    
-    fileprivate static func indexShortnames() -> [String: Int] {
-        let emojis = Emoji.allCases
-        return emojis.reduce([String: Int](), { dict, emoji -> [String: Int] in
-            guard let index = emojis.firstIndex(of: emoji) else { return [:] }
-            return emoji.shortnames.reduce(dict, { eDict, shortname -> [String: Int] in
+
+    fileprivate static func createIndex(for propertyFunction: (Emoji) -> [String]) -> [String: Int] {
+        Emoji.allCases.reduce([String: Int](), { dict, emoji -> [String: Int] in
+            guard let index = Emoji.allCases.firstIndex(of: emoji) else { return [:] }
+            return propertyFunction(emoji).reduce(dict, { eDict, shortname -> [String: Int] in
                 var finalDict = eDict
                 finalDict[shortname] = index
                 return finalDict
             })
         })
     }
-    
-    fileprivate static func indexCodepoints() -> [String: Int] {
-        let emojis = Emoji.allCases
-        return emojis.reduce([String: Int](), { dict, emoji -> [String: Int] in
-            guard let index = emojis.firstIndex(of: emoji) else { return [:] }
-            return emoji.codepoints.reduce(dict, { eDict, codepoint -> [String: Int] in
-                var finalDict = eDict
-                finalDict[codepoint] = index
-                return finalDict
-            })
-        })
-    }
-    
+
     public var emojiUnescapedString: String {
         var s = self as NSString
         let ms = String.emojiUnescapeRegExp?.matches(in: self, options: [], range: NSMakeRange(0, s.length))
@@ -73,8 +62,7 @@ extension String {
             let r = m.range
             let p = s.substring(with: r)
             let px = p[p.index(after: p.startIndex) ..< p.index(before: p.endIndex)]
-            let index = String.indexedShortnames[String(px)]
-            if let i = index {
+            if let i = String.indexedShortnames[String(px)] {
                 let e = Emoji.allCases[i]
                 s = s.replacingCharacters(in: r, with: e.codepoints.first!) as NSString
             }
@@ -88,8 +76,7 @@ extension String {
         ms?.reversed().forEach { m in
             let r = m.range
             let p = s.substring(with: r)
-            let index = String.indexedCodepoints[p]
-            if let i = index {
+            if let i = String.indexedCodepoints[p] {
                 let e = Emoji.allCases[i]
                 s = s.replacingCharacters(in: r, with: ":\(e.shortnames.first!):") as NSString
             }
